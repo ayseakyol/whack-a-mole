83168a6eca07b6fea59b6a5fc6f55dd1444ab793
ayseakyol <ayseakyol.22309@gmail.com>
2020-04-12 15:10:49 +0200

    ok

--- app/mole.js
+++ app/mole.js
@@ -1,8 +1,7 @@
 class Mole {
-
-  timeoutId = null
-  isVisible = false
-  isWhacked = false
+  timeoutId = null;
+  isVisible = false;
+  isWhacked = false;
 
   static hideDelay() {
     return Math.floor(Math.random() * 3000) + 500;
@@ -13,12 +12,39 @@ class Mole {
   }
 
   handleClick(view) {
+    if (this.isWhacked) {
+      clearTimeout(this.timeoutId);
+    } else {
+      this.timeoutId = setTimeout(this.timeoutCallback.bind(this, view), 0);
+    }
+    this.isWhacked = !this.isWhacked;
+
+    this.log.push({
+      isWhacked: this.isWhacked,
+      timeoutId: this.timeoutId,
+      isVisible: this.isVisible,
+    });
   }
 
   timeoutCallback(view) {
+    if (this.isWhacked) {
+      this.isVisible = !this.isVisible;
+      view.innerText = this.isVisible ? "  \n  \n  " : this.innerText;
+      this.timeoutId = setTimeout(this.timeoutCallback.bind(this, view), 0);
+    }
   }
 
   render() {
+    const container = document.createElement("div");
+    container.style =
+      "height: 100%;" +
+      "width: 100%;" +
+      "display: flex;" +
+      "align-items: center;" +
+      "justify-content: center;";
+    container.innerText = this.innerText;
+    container.onclick = this.handleClick.bind(this, container);
+    this.timeoutId = setTimeout(this.timeoutCallback.bind(this, container), 0);
+    return container;
   }
-
 }
